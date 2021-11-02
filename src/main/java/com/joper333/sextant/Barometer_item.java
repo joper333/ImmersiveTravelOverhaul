f7585879602c4df08a776421f2bbfe2333348c76
joper333 <joper320@gmail.com>
2021-11-01 23:27:54 -0400

    barometer now has a delay of 1.5 seconds. added sounds.

--- src/main/java/com/joper333/sextant/Barometer_item.java
+++ src/main/java/com/joper333/sextant/Barometer_item.java
@@ -4,14 +4,18 @@ import java.lang.*;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -25,34 +29,50 @@ public class Barometer_item extends Item{
         super(settings);
     }
 
+    public int getMaxUseTime(ItemStack stack) {
+        return 25;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BLOCK;
+    }
+
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, playerEntity, hand);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        PlayerEntity playerEntity = user.getEntityWorld().getClosestPlayer(user, 1);
         if (world.isClient) {
+            playerEntity.playSound(SoundEvents.BLOCK_LAVA_POP, 0.7F, 1.2F);
+            playerEntity.playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 0.6F);
             int Y = playerEntity.getBlockPos().getY();
             int seaLevel = Y - 63;
-          if(seaLevel < 0)
-          {
-              if (seaLevel == -1)
-              {
-                  playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meter below sea level"), true);
+            if(seaLevel < 0)
+            {
+                if (seaLevel == -1)
+                {
+                    playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meter below sea level"), true);
 
-              }else {playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meters below sea level"), true); }
+                }else {playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meters below sea level"), true); }
 
-          }else if (seaLevel > 0)
-          {
-              if (seaLevel == 1)
-              {
-                  playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meter above sea level"), true);
+            }else if (seaLevel > 0)
+            {
+                if (seaLevel == 1)
+                {
+                    playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meter above sea level"), true);
 
-              }else{playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meters above sea level"), true); }
+                }else{playerEntity.sendMessage(new TranslatableText("I'm " + Math.abs(seaLevel) +" meters above sea level"), true); }
 
-          }else{playerEntity.sendMessage(new TranslatableText("I'm at sea level"), true);}
+            }else{playerEntity.sendMessage(new TranslatableText("I'm at sea level"), true);}
 
             //playerEntity.sendMessage(new TranslatableText(String.valueOf(seaLevel)), true);
         }
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+        return stack;
     }
-    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+
+        public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
         tooltip.add(new TranslatableText("item.sextant.barometer.tooltip").formatted(Formatting.WHITE));
     }
 }
