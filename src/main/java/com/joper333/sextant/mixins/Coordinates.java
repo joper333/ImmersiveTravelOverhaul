676cb9e524dee499fd2a6bdc7fe0eed942cd93a2
joper333 <joper320@gmail.com>
2022-11-20 20:48:32 -0500

    -updated to 1.19.2

--- src/main/java/com/joper333/sextant/mixins/Coordinates.java
+++ src/main/java/com/joper333/sextant/mixins/Coordinates.java
@@ -1,13 +1,18 @@
 package com.joper333.sextant.mixins;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.DebugHud;
 
 
 import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Locale;
@@ -16,26 +21,27 @@ import java.util.Locale;
 @Mixin (DebugHud.class)
 public abstract class Coordinates extends DrawableHelper {
 
+
     //i think this can be done better with slices? also how fix ugly gap in the menu, idk
     @Redirect(
             method = "getLeftText",
-            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/util/Locale;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 0))
+            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/util/Locale;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 3))
     private String XYZ(Locale l, String format, Object[] args) {
-        return null;
+        return "";
     }
 
     @Redirect(
             method = "getLeftText",
-            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 3))
-    private String Block(String format, Object[] args) {
-        return null;
+            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/util/Locale;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 4))
+    private String Block(Locale l, String format, Object[] args) {
+        return "";
     }
 
     @Redirect(
             method = "getLeftText",
-            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 4))
-    private String Chunk(String format, Object[] args) {
-        return null;
+            at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/util/Locale;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", ordinal = 5))
+    private String chunk(Locale l, String format, Object[] args) {
+        return "";
     }
 
     //this is a horrible way to do this, and it looks ugly, but I have no idea on how to do this another way. plz help
@@ -44,6 +50,7 @@ public abstract class Coordinates extends DrawableHelper {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;getZ()I"))
     private int Z(BlockPos instance) {
         return 0;
+        //(int) Math.floor(Math.random() * 9) <-- could return this instead if it didn't run every tick
     }
 
     @Redirect(
